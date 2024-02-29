56a38cbfcd785409e55526fa71c8d5f957cb393c
andyzheng679 <andyzheng679@gmail.com>
2024-02-29 12:38:33 -0500

    create for loop to add all the numbers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  * Modified by kristofer in Jan 2024
  */
+//import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
@@ -16,12 +19,17 @@ public class Main {
     
     // do this one and the lab is complete. really.
     // return the sum of 0 to n... if n == 3, the result should be 6
-    int sumOfNumbers(int n) {
-        return 0;
+    public static int sumOfNumbers(int n) {
+    //for loop going over and adding it all up, create a variable
+        int total_sum = 0;
+        for(int i = 1; i <= n; i++){
+            total_sum += i;
+        }
+        return total_sum;
     }
     
     
-    int getInputNumber() { // gee, may thi sis useful in another lab?
+    public static int getInputNumber() { // gee, may thi sis useful in another lab?
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter any number: ");
 
